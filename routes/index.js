b0238cc49534798ddd04d56ea809b1b5fa94b699
Aayush Chhabra <aayushgen@gmail.com>
2021-02-20 12:32:14 -0800

    resume

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/')
+router.get('/resume/download', function(req, res, next) {
+  res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/export?format=pdf")
+})
+
+router.get('/resume', function(req, res, next) {
+  res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/edit?usp=sharing")
+})
 
 module.exports = router;
