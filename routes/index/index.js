f3dcd90c456878160590af1252f99548abcb5114
Aayush Chhabra <aayushgen@gmail.com>
2021-02-20 13:58:26 -0800

    about route

--- routes/index/index.js
+++ routes/index/index.js
@@ -5,12 +5,18 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/resume/download', function(req, res, next) {
-  res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/export?format=pdf")
-})
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
 
 router.get('/resume', function(req, res, next) {
   res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/edit?usp=sharing")
 })
+
+router.get('/resume/download', function(req, res, next) {
+  res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/export?format=pdf")
+})
+
+
 
 module.exports = router;
