b77eda55c4398c6edaf547d702fd553f9a66c9ac
Aayush Chhabra <aayushgen@gmail.com>
2021-02-20 14:38:26 -0800

    basic routes

--- routes/index/index.js
+++ routes/index/index.js
@@ -9,8 +9,18 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
+router.get('/books', function(req, res, next) {
+  res.redirect("https://www.goodreads.com/user/show/66380699-aayush-chhabra");
+});
+
+https://www.goodreads.com/user/show/66380699-aayush-chhabra
+
 router.get('/resume', function(req, res, next) {
   res.redirect("https://docs.google.com/document/d/1k8zGr_zN5rjhv-15htfcNT2NLB0ZCQZ7CzRDW8CRIkI/edit?usp=sharing")
+})
+
+router.get('/resume2', function(req, res, next) {
+  res.render('resume');
 })
 
 router.get('/resume/download', function(req, res, next) {
